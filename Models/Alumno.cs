e7e60967a9d41c43bf3a785d2b04e26e0c518357
DavidNolasco <david_nolasco@usmp.pe>
2025-08-04 15:43:39 -0500

    inicio

---
+++ Models/Alumno.cs
@@ -0,0 +1,9 @@
+namespace Ver_Estado_Admision{
+
+    public class Alumno
+    {
+        string nombre { get; set; }
+        string apellidoPaterno { get; set; }
+        string apellidoMaterno { get; set; }
+    }
+}
